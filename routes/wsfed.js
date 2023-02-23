85e8f4726b8464949032d96ebc10cc5fbfc49313
Armin Felder <armin.felder@gmail.com>
2023-02-23 21:54:31 +0100

    handle invlaid login requests

--- routes/wsfed.js
+++ routes/wsfed.js
@@ -25,18 +25,25 @@ const router = express.Router();
 
 
 router.get('/',(req,res,next)=>{
-    if(req.query.hasOwnProperty("wa")&& req.query.wa === "wsignout1.0") {
+    if(req.query.hasOwnProperty("wa")&& req.query.wa === "wsignout1.0") { // user requests a logout
         res.redirect(req.app.get("SAML2_ROOT") + "/logout");
-    }
-    else if(req.isAuthenticated() && req.session.hasOwnProperty("wsfed_args")){
+    }else if(req.isAuthenticated() && req.session.hasOwnProperty("wsfed_args")){ // user has been authenticated and his session contains the required arguments for WSFED to proceed
         const sessData = req.session;
         req.query = sessData.wsfed_args
         next();
-    }else {
+    }else if ( req.query.hasOwnProperty("wa") && req.query.hasOwnProperty("wtrealm") ){   // user does is not logged in and requests a login
         const sessData = req.session;
         sessData.wsfed_args = Object.assign({},req.query);
         req.session.save();
         res.redirect(req.app.get("SAML2_ROOT") + "/login");
+    }else if(req.isAuthenticated()) { // user is authenticated, but now valid session data is present, destroy the session as it is not valid
+        res.redirect(req.app.get("SAML2_ROOT") + "/logout");
+    }else { // user is neither authenticated nor does he present valid WSFED arguments
+        if (req.app.get("INVALID_LOGIN_REDIRECT") !== ""){
+            res.redirect(303, req.app.get("INVALID_LOGIN_REDIRECT"))
+        }else{
+            res.sendStatus(400)
+        }
     }
 },(req,res,next)=>{
    return wsfed.auth({
